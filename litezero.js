dfe54856b96bf2549e390da144278163afac76c5
Ezequiel Alonzo <ezequielalonzo21@gmail.com>
2024-04-09 11:27:30 -0500

    Create litezero.js

---
+++ litezero.js
@@ -0,0 +1,25 @@
+function setlzModal() {
+  $('<div class="lz_modal"><div class="lz_modal_content"></div></div>').appendTo('body');
+}
+
+$('[data-lz-modal]').click(function() {
+  var lzImg = $(this).attr('src')
+  var lzTitle = $(this).attr('data-lz-title')
+	setlzModal()
+  $('<img class="lz_modal_img" src="' + lzImg + '">').appendTo('.lz_modal_content');
+  if (lzTitle !== undefined) {
+    $('<span class="lz_modal_title">' + lzTitle + '</span>').appendTo('.lz_modal_content');
+  }
+  $('.lz_modal').fadeIn(250);
+})
+
+$('body').on('click', '.lz_modal', function(event) {
+  $('.lz_modal').fadeOut(250);
+  setTimeout(function() {
+    $('.lz_modal').remove();
+  }, 300)
+});
+
+$('body').on('click', '.lz_modal_img, .lz_modal_title', function(event) {
+  event.stopPropagation();
+});
