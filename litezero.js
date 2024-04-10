6f40c2962436487490c4a97ad8963b9d88afd767
Ezequiel Alonzo <ezequielalonzo21@gmail.com>
2024-04-10 08:22:52 -0500

    Update litezero.js

--- litezero.js
+++ litezero.js
@@ -10,11 +10,13 @@ $('[data-lz-modal]').click(function() {
   if (lzTitle !== undefined) {
     $('<span class="lz_modal_title">' + lzTitle + '</span>').appendTo('.lz_modal_content');
   }
+$('body').addClass('lz_overhide');
   $('.lz_modal').fadeIn(250);
 })
 
 $('body').on('click', '.lz_modal', function(event) {
   $('.lz_modal').fadeOut(250);
+$('body').removeClass('lz_overhide');	
   setTimeout(function() {
     $('.lz_modal').remove();
   }, 300)
